1e901d25aa0220ff1f372288b56b551382759d2c
River Schenck <riverschenck@mail.weber.edu>
2018-05-20 23:08:24 -0600

    fixed

--- NTPtimeESP/NTPtimeESP.ino
+++ NTPtimeESP/NTPtimeESP.ino
@@ -1,11 +1,4 @@
-/*
-   This sketch shows an example of sending a reading to data.sparkfun.com once per day.
 
-   It uses the Sparkfun testing stream so the only customizing required is the WiFi SSID and password.
-
-   The Harringay Maker Space
-   License: Apache License v2
-*/
 #include <NTPtimeESP32.h>
 
 #define DEBUG_ON
@@ -15,20 +8,7 @@ NTPtime NTPch("ch.pool.ntp.org");   // Choose server pool as required
 char *ssid      = "Schenck Family";               // Set you WiFi SSID
 char *password  = "Ridger1990";               // Set you WiFi password
 
-/*
- * The structure contains following fields:
- * struct strDateTime
-{
-  byte hour;
-  byte minute;
-  byte second;
-  int year;
-  byte month;
-  byte day;
-  byte dayofWeek;
-  boolean valid;
-};
- */
+
 strDateTime dateTime;
 
 void setup() {
@@ -48,11 +28,8 @@ void setup() {
 
 void loop() {
 
-  // first parameter: Time zone in floating point (for India); second parameter: 1 for European summer time; 2 for US daylight saving time; 0 for no DST adjustment; (contributed by viewwer, not tested by me)
   dateTime = NTPch.getNTPtime(1.0, 1);
 
-  // check dateTime.valid before using the returned time
-  // Use "setSendInterval" or "setRecvTimeout" if required
   if(dateTime.valid){
     NTPch.printDateTime(dateTime);
 
